f022a788151421e139174434b4fa984409218539
GreatPeng <1193325271@qq.com>
2019-01-06 15:25:48 +0800

    Initial Commit

---
+++ CollectionViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewDemo
+//
+//  Created by admin on 2019/1/6.
+//  Copyright © 2019年 wangpeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
